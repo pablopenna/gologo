0b9097221075d6f2fe62fd0dc9d81e7fd4114201
pablo <pablo_penna7@hotmail.com>
2022-08-07 15:59:37 +0100

    Tweaked player movement and number of player projectiles on screen

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 export var direction = Vector2.UP
 export var speed = 100
+
+signal outOfScreen
 	
 func _process(delta):
 	position += direction * speed * delta
@@ -9,3 +11,4 @@ func _process(delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+	emit_signal("outOfScreen")
--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -3,14 +3,24 @@ extends Node2D
 export var speed = 150
 export(PackedScene) var projectileScene
 
+export var shootingCooldown = 0.05
+export var projectileSpeed = 750
+export var maxProjectiles = 3
+var currentProjectiles
+
 func _ready():
-	pass
+	currentProjectiles = 0
+	$ShootingTimer.start(shootingCooldown)
 	
 func _process(delta):
 	_processPosition(delta)
-	if Input.is_action_just_pressed("player_shoot"):
+	if Input.is_action_just_pressed("player_shoot") && $ShootingTimer.time_left == 0 && currentProjectiles < maxProjectiles:
 		shoot()
 	
+func _processPosition(delta):
+	position.x += _getMovementInput() * speed * delta
+	position.x = clamp(position.x, 0, get_viewport_rect().size.x)	
+
 func _getMovementInput():
 	var movement = 0
 	if Input.is_action_pressed("player_left"):
@@ -18,18 +28,19 @@ func _getMovementInput():
 	if Input.is_action_pressed("player_right"):
 		movement += 1
 	return movement
-
-func _getMovement():
-	return _getMovementInput() * speed
-	
-func _processPosition(delta):
-	position.x += _getMovement() * delta
-	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
 	
 func shoot():
-	print_debug("shoot!")
+	$ShootingTimer.start(shootingCooldown)
+	currentProjectiles+=1
+	
 	var projectile = projectileScene.instance()
 	projectile.direction = Vector2.UP
+	projectile.speed = projectileSpeed
 	projectile.position = position
+	projectile.connect("outOfScreen", self, "_projectileDestroyed")
+	
 	get_tree().root.add_child(projectile)
 
+func _projectileDestroyed():
+	currentProjectiles-=1
+	
