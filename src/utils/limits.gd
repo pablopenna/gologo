8bec8288d04e981392b933b1b670acc4862ca3d5
Pablo <pablo_penna7@hotmail.com>
2024-08-20 21:30:07 +0100

    Make Limit adjust to viewport size

--- src/utils/limits.gd
+++ src/utils/limits.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
+@export var shape: CollisionShape2D
+@export var adjust_shape_to_viewport: bool
+
 func _ready():
 	self.area_exited.connect(_handle_area_exited)
+	if adjust_shape_to_viewport and shape:
+		var viewport_size_shape = RectangleShape2D.new()
+		viewport_size_shape.size = get_viewport_rect().size
+		shape.shape = viewport_size_shape
 
 func _handle_area_exited(area: Area2D):
 	if area as Projectile:
