8435a2b37f5af499aeb2c091568691c29325545c
Pablo <pablo_penna7@hotmail.com>
2024-08-25 11:28:31 +0100

    refactor projectile collision fix using raycast

--- src/projectile/Projectile.gd
+++ src/projectile/Projectile.gd
@@ -1,39 +1,38 @@
 class_name Projectile extends Area2D
 
-@export var fix_collision_shape: CollisionShape2D
-var position_in_past_frame: Vector2
+@export var fix_collision_raycast: RayCast2D
 
 @export var direction: Vector2 = Vector2.UP
 @export var speed: int = 1
 var _hasExploded: bool = false
+var _is_out_of_screen: bool = false
 
 signal outOfScreen
 signal destroyed # when destroying the projectile via queue_free(), VisibleOnScreenNotifier3D is triggered
 
 func _ready() -> void:
-	position_in_past_frame = position
+	fix_collision_raycast.collision_mask = self.collision_mask
+	area_entered.connect(_on_Projectile_area_entered)
 	
 func _physics_process(delta: float) -> void:
-	position += direction * speed * delta
-	_update_fix_collision_shape()
+	var original_position = Vector2(global_position)
+	global_position += direction * speed * delta
+	_update_fix_collision_raycast(original_position)
+	if fix_collision_raycast.is_colliding():
+		_on_Projectile_area_entered(fix_collision_raycast.get_collider())
+	if _is_out_of_screen:
+		_destroy()
 
-func _update_fix_collision_shape() -> void:
-	var shape = fix_collision_shape.shape as RectangleShape2D
-	var distance_to_last_position = abs(position.distance_to(position_in_past_frame))
-	shape.size.y = distance_to_last_position
-	fix_collision_shape.position.y -= (direction.y * distance_to_last_position)/2
-	
-	position_in_past_frame = position
+func _update_fix_collision_raycast(raycast_position: Vector2) -> void:
+	fix_collision_raycast.global_position = raycast_position
+	fix_collision_raycast.target_position = raycast_position.direction_to(global_position) * raycast_position.distance_to(global_position)
 
 func _destroy():
 	destroyed.emit()
 	call_deferred("queue_free")
 
 func _on_VisibilityNotifier2D_screen_exited():
-	if not _hasExploded:
-		await get_tree().create_timer(2).timeout
-		outOfScreen.emit()
-		_destroy()
+	_is_out_of_screen = true
 
 func _on_Projectile_area_entered(area):
 	var hit_entity := area as DamageableEntity
