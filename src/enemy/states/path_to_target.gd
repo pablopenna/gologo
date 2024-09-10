84eebd87fc13e36977d511f1cb60d6ea1c30438b
Pablo <pablo_penna7@hotmail.com>
2024-09-10 14:15:32 +0100

    Add some more points to enemy path

--- src/enemy/states/path_to_target.gd
+++ src/enemy/states/path_to_target.gd
@@ -33,12 +33,22 @@ func _create_path_to_target() -> void:
 	var curve = Curve2D.new()
 	path.curve = curve
 	
-	curve.add_point(managed_enemy.global_position)
-	curve.add_point(managed_enemy.target.global_position)
+	var origin = managed_enemy.global_position
+	var destination = managed_enemy.target.global_position
+	var middle_point_1 = origin + (destination - origin)*0.3 + Vector2.RIGHT*50
+	var middle_point_2 = origin + (destination - origin)*0.6 + Vector2.RIGHT*50
+	var middle_point_3 = origin + (destination - origin)*0.9 + Vector2.LEFT*50
+	
+	curve.add_point(origin)
+	curve.add_point(middle_point_1,  Vector2(-50, -10), Vector2(50, 10))
+	curve.add_point(middle_point_2,  Vector2(-50, -10), Vector2(50, 10))
+	curve.add_point(middle_point_3,  Vector2(-50, -10), Vector2(50, 10))
+	curve.add_point(destination)
 	
 	add_child(path)
 	_path_follow.reparent(path)
 	_path_created = true
+
 
 func _on_reached_destination() -> void:
 	change_to_state.emit(on_reached_destination_state.state_name, {})
